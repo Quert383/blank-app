7fc98b63109dcec95f26321e35d067620fc567a9
Hubert383 <domhuber99@gmail.com>
2025-05-03 13:30:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,24 +23,24 @@ def main():
     wyplaty = []
     for i in range(int(m)):
         st.subheader(f"Wypłata nr {i+1}")
-        ck = st.number_input(f"Kwota wypłaty nr {i+1}:", key=f"ck_{i}")
-        tk = st.number_input(f"Czas wypłaty nr {i+1} (w latach, np. 0 dla wypłaty dzisiaj):", key=f"tk_{i}", format="%.6f")
+        ck = st.number_input(f"Kwota wypłaty nr {i+1}:", key=f"ck_{i}", format="%.2f")
+        tk = st.number_input(f"Czas wypłaty nr {i+1} (w latach, np. 0 dla wypłaty dziś):", key=f"tk_{i}", format="%.6f")
         wyplaty.append((ck, tk))
 
     st.header("Dane o ratach")
 
-    rata_stala = st.number_input("Podaj wysokość stałej raty (zł):", min_value=0.0, step=0.01, format="%.2f")
-    liczba_rat_stalych = st.number_input("Podaj liczbę stałych rat:", min_value=0, step=1)
-    rata_ostatnia = st.number_input("Podaj wysokość ostatniej raty (wyrównawczej):", min_value=0.0, step=0.01, format="%.2f")
+    rata_stala = st.number_input("Wysokość stałej raty (zł):", min_value=0.0, step=0.01, format="%.2f")
+    liczba_rat_stalych = st.number_input("Liczba stałych rat:", min_value=0, step=1)
+    rata_ostatnia = st.number_input("Wysokość ostatniej raty (wyrównawczej):", min_value=0.0, step=0.01, format="%.2f")
 
     splaty = []
     # Dodajemy stałe raty co miesiąc
     for i in range(int(liczba_rat_stalych)):
-        czas = (i + 1) / 12  # pierwszy miesiąc = 1/12 roku, potem 2/12 itd.
+        czas = (i + 1) / 12  # pierwsza rata za 1 miesiąc
         splaty.append((rata_stala, czas))
 
     # Dodajemy ostatnią ratę
-    czas_ostatniej_raty = (liczba_rat_stalych + 1) / 12  # kolejny miesiąc po stałych ratach
+    czas_ostatniej_raty = (liczba_rat_stalych + 1) / 12
     splaty.append((rata_ostatnia, czas_ostatniej_raty))
 
     if st.button("Oblicz RRSO"):
